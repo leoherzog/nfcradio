cd47cb3ae86f3fc2639e309a0d2a373f98490418
Leo Herzog <xd1936@gmail.com>
2022-12-29 21:41:54 -0500

    Add More Error Handling and Graceful Shutdown

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#/usr/bin/python3
+
 import os
 import multiprocessing
 import urllib.request
@@ -13,31 +15,45 @@ def main():
 
   while True: # main loop
 
-    tagid = input("NFC Tag ID: ") # user input
-    
-    correspondingid = urllib.request.urlopen("https://script.google.com/macros/s/AKfycbw9KcSfkivP_ZJG53Ix-2fr-Vkk63KZK7Wfsj10hkNvAOu2XddJy8xjVJSOO4HZKqk3/exec?id=" + tagid).read().decode("utf-8")
-
     try:
-      yt = YouTube(correspondingid) # try to get the video
-    except:
-      correspondingid = "https://www.youtube.com/watch?v=" + correspondingid # if it's not a URL
-      try:
-        yt = YouTube(correspondingid) # try to get the video with the ID
-      except:
-        print("No ID found")
-        continue
-      
-    print("Downloading...")
-    downloadpath = yt.streams.filter(only_audio=True).order_by("abr").desc().first().download("cache") # highest quality audio
-    print("Downloaded!")
 
-    if playing is not None:
+      tagid = input("NFC Tag ID: ") # user input
+      if tagid == "": # if the user just presses enter
+        tagid = "00BAF91F04" # for testing
+      
+      correspondingid = urllib.request.urlopen("https://script.google.com/macros/s/AKfycbw9KcSfkivP_ZJG53Ix-2fr-Vkk63KZK7Wfsj10hkNvAOu2XddJy8xjVJSOO4HZKqk3/exec?id=" + tagid).read().decode("utf-8")
+
+      try:
+        yt = YouTube(correspondingid) # try to get the video
+      except:
+        correspondingid = "https://www.youtube.com/watch?v=" + correspondingid # if it's not a URL
+        try:
+          yt = YouTube(correspondingid) # try to get the video with the ID
+        except:
+          print("No ID found")
+          continue
+        
+      print("Downloading...")
+      downloadpath = yt.streams.filter(only_audio=True).order_by("abr").desc().first().download("cache") # highest quality audio
+      print("Downloaded!")
+
+      if playing is not None:
+        print("Stopping...")
+        playing.terminate() # stop the current process
+      
+      print("Playing...")
+      playing = multiprocessing.Process(target=playsound, args=(downloadpath,)) # create a new process
+      playing.start() # and start it
+
+    except KeyboardInterrupt:
       print("Stopping...")
-      playing.terminate() # stop the current process
-    
-    print("Playing...")
-    playing = multiprocessing.Process(target=playsound, args=(downloadpath,)) # create a new process
-    playing.start() # and start it
+      if playing is not None:
+        playing.terminate()
+      break
+
+    except Exception as e:
+      print("Error:" + e)
+      continue
 
 if __name__ == '__main__':
   main()
